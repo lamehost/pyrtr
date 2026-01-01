e64f474bc8436dbaa07658060cad00faae378e48
Marco Marzetti <mmarzetti@pccwglobal.com>
2026-01-01 12:36:17 +0100

    Fixed multiple typos affecting validation

--- pyrtr/settings.py
+++ pyrtr/settings.py
@@ -29,14 +29,14 @@ class Settings(BaseSettings):
     LOGLEVEL: LogLevelEnums = LogLevelEnums.INFO
 
     HOST: IPv4Address | IPv6Address = IPv4Address("127.0.0.1")
-    PORT: Annotated[int, Field(gt=0, lt=65536)] = 8323  # pyright: ignore[reportCallIssue]
+    PORT: Annotated[int, Field(gt=0, lt=65536)] = 8323
     JSONFILE: str | os.PathLike[str] = "json"
-    RELOAD: Annotated[int, Field(gt=0, ls=3600)] = 900  # pyright: ignore[reportCallIssue]
+    RELOAD: Annotated[int, Field(gt=0, lt=3600)] = 900
 
     # https://datatracker.ietf.org/doc/html/rfc8210#section-6
-    REFRESH: Annotated[int, Field(gt=0, ls=86401)] = 3600  # pyright: ignore[reportCallIssue]
-    RETRY: Annotated[int, Field(gt=0, ls=7201)] = 600  # pyright: ignore[reportCallIssue]
-    EXPIRE: Annotated[int, Field(gt=599, ls=172801)] = 7200  # pyright: ignore[reportCallIssue]
+    REFRESH: Annotated[int, Field(gt=0, lt=86401)] = 3600
+    RETRY: Annotated[int, Field(gt=0, lt=7201)] = 600
+    EXPIRE: Annotated[int, Field(gt=599, lt=172801)] = 7200
 
     model_config = SettingsConfigDict(env_prefix="PYRTR_")
 
